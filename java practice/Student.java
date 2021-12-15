908c6d164e0890f8415aaf159d868e2b0c0515b4
uday8166 <udaybandla81@gmail.com>
2021-12-15 15:49:49 +0530

    This is JAva Practice

---
+++ java practice/Student.java
@@ -0,0 +1,9 @@
+class Student{
+
+Student(){
+this(23,"Uday");
+}
+Student(int age,String name){
+this();
+}
+}
---
+++ java practice/vegetable.java
@@ -0,0 +1,16 @@
+package math;
+
+
+
+public class vegetable {
+	int i;
+	String name;
+public 
+	vegetable v= new vegetable();
+	v.i = 120;
+	v.name = "Carrot";
+	 
+System.out.println(+i" "+name);
+	
+
+}
